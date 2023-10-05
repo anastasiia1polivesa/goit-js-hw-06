5e32329988058c18cdca812f3c8bff6d4bdc8a18
anastasiia1polivesa <136483100+anastasiia1polivesa@users.noreply.github.com>
2023-10-05 17:56:10 +0300

    Update task-01.js

---
+++ js/task-01.js
@@ -0,0 +1,9 @@
+
+const categories = document.querySelectorAll("#categories li.item");
+const numberofcategories = categories.length;
+console.log(numberofcategories);
+
+categories.forEach(
+title = document.querySelector("h2"));
+console.log(title.textContent) => {
+}
